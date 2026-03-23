90ec975dd1fcf3136cb8b4c4ff4174cd0a477dd2
sindabenchaabane <ben.chaabane.sinda98@gmail.com>
2026-03-23 10:11:51 +0100

    commit2

--- import html.py
+++ import html.py
@@ -7,3 +7,12 @@ html.escape("This is a test & only a test.", quote=True)
 # 'This is a test &amp; only a test.'
 html.escape('This is a "test" & only a test.', quote=True)
 # 'This is a &quot;test&quot; &amp; only a test.'
+html.unescape('This is a &quot;test&quot; &amp; only a test.')
+# 'This is a "test" & only a test.'
+
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()   
